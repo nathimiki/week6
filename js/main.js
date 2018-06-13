bdbdad493f859230c85a64f75a9e18678c12e589
nathimiki <nathimiki@gmail.com>
2018-06-13 14:00:48 -0400

    added in js var, functions...

--- js/main.js
+++ js/main.js
@@ -4,15 +4,24 @@
 
 
 
-	//Vars
+//Vars
+var num1 = document.querySelector("#input1");
+var num2 = document.querySelector("#input2");
+var equals = document.querySelector("#trigger");
+var outcome = document.querySelector("#value");
+/*console.log(num1);
+console.log(num2);
+console.log(equals);
+console.log(outcome);*/
+
+//Functions
+function addStuff(){
+	//console.log("from addStuff");
+}
 
 
-
-	//Functions
-
-
-
-	//Listeners
+//Listeners
+equals.addEventListener("click", addStuff, false);
 
 
 })();
